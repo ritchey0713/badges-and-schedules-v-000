b7a6701a60ae2bbe41690df94d4fab2fb516e367
David Ritchey <ritchey0713@gmail.com>
2018-03-23 16:25:27 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,11 +1,13 @@
 def badge_maker(name)
   return "Hello, my name is #{name}."
+
 end
 
 def batch_badge_creator(array)
   array.collect do |names|
     badge_maker(names) 
   end
+
 end
 
 def assign_rooms(array)
@@ -13,6 +15,8 @@ def assign_rooms(array)
   array.collect do |names|
     room+=1
     "Hello, #{names}! You'll be assigned to room #{room}!"
+end
 
 end
-end
+
+def 
